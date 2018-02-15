0b764603ad57986b6467cbff06ebc08544f862c6
Gautineo <gauravsati1996@gmail.com>
2018-02-15 16:25:40 +0530

    added request field and submit button

--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/UnscheduledRequest.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/UnscheduledRequest.java
@@ -55,18 +55,18 @@ public class UnscheduledRequest extends AppCompatActivity {
 //        });
         //btnTimePicker.setOnClickListener(this);
 
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 //        Button pickDateTime  = findViewById(R.id.button_pickDateTime);
 //        pickDateTime.setOnClickListener(new View.OnClickListener() {
 //            @Override
