141f1094de7974a7a57998ca9d1fe877d45f3408
Gautineo <gauravsati1996@gmail.com>
2018-02-18 01:22:51 +0530

    Added the Help and support button

--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/MainRequestActivity.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/MainRequestActivity.java
@@ -4,6 +4,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainRequestActivity extends AppCompatActivity {
 
@@ -18,5 +20,16 @@ public class MainRequestActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.slider_view);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.request_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
 }
